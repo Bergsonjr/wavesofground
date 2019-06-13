045d12754d8a2c7c38e1e7a581d984236f78a7a5
Bergson <bergsonjr@icloud.com>
2019-06-12 23:50:52 -0300

    Hello listComponente

---
+++ src/app/blog/list/PostListComponent.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: 'app-post-list',
+    templateUrl: 'PostList.html'
+})
+
+export class PostListComponent implements OnInit {
+    constructor() {
+
+    }
+
+    ngOnInit() {
+
+    }
+}
---
+++ src/app/blog/list/PostListModule.ts
@@ -0,0 +1,12 @@
+import { PostListComponent } from './PostListComponent';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  imports: [],
+  exports: [PostListComponent],
+  declarations: [PostListComponent],
+  providers: []
+})
+
+export class PostListModule {
+}
